176a841c74e0a48206386a22f102c013db66460b
KJAVA19811815 <karunsingh1993@gmail.com>
2017-10-06 17:46:52 +0000

    can create menus and show them inthe index file

--- server.js
+++ server.js
@@ -15,6 +15,42 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/menus", (req, res) => {
+  return knex("menus")
+    .select()
+    .then(function(menus) {
+      console.log(menus);
+      res.render("index", { menus: menus });
+    });
+});
+
+app.get("/menus/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/menus", (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let price = req.body.price;
+  let description = req.body.description;
+
+  knex("menus")
+    .insert({
+      name: name,
+      image: image,
+      price: price,
+      description: description
+    })
+    .then(function(result) {
+      console.log("it works");
+      res.redirect("/menus");
+    });
+});
+
 app.listen(3000, () => {
   console.log("server at 3000");
 });
